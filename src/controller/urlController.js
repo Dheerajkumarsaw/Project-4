04e11efe4194d1b5132eae9cf7de7d2d3794f1c2
Dheeraj Kumar Saw <dheerajkumarsaw99@gmail.com>
2022-05-18 15:14:13 +0530

    caching connected

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -1,12 +1,51 @@
 const urlModel = require("../model/urlModel");
 const nanoId = require("nanoid")
-const validUrl = require("valid-url")
+const validUrl = require("valid-url");
+const redis = require("redis");
+const { promisify } = require("util");
+
 
 const isValid = function (value) {
     if (typeof value === "undefined" || typeof value === null) return false
     if (typeof value === "string" && value.trim().length == 0) return false
     return true
 }
+
+
+//Connect to redis
+const redisClient = redis.createClient(
+    13190,
+    "redis-13190.c301.ap-south-1-1.ec2.cloud.redislabs.com",
+    { no_ready_check: true }
+);
+redisClient.auth("gkiOIPkytPI3ADi14jHMSWkZEo2J5TDG", function (err) {
+    if (err) throw err;
+});
+
+redisClient.on("connect", async function () {
+    console.log("Connected to Redis..");
+});
+
+//1. connect to the server
+//2. use the commands :
+
+//Connection setup for redis
+
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
+
+const fetchAuthorProfile = async function (req, res) {
+    let cahcedProfileData = await GET_ASYNC(`${req.params.authorId}`)
+    if (cahcedProfileData) {
+        res.send(cahcedProfileData)
+    } else {
+        let profile = await authorModel.findById(req.params.authorId);
+        await SET_ASYNC(`${req.params.authorId}`, JSON.stringify(profile))
+        res.send({ data: profile });
+    }
+
+};
+
 
 const createUrl = async function (req, res) {
     try {
